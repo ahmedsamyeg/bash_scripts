3a4eedaaabdacc9712441fce9d7db7318214b1ca
Ahmed Elsayed <me@ahmedsamy.net>
2019-05-30 08:15:10 -0700

    Add install.sh, install new commands

---
+++ src/install.sh
@@ -0,0 +1,11 @@
+
+# copy the commands files.
+sudo cp -i runmyvm.sh /usr/local/bin/runmyvm
+sudo cp -i runvm.sh /usr/local/bin/runvm
+sudo cp -i sysinfo.sh /usr/local/bin/sysinfo
+
+# apply read and execute permissions.
+
+chmod +xr /usr/local/bin/runmyvm
+chmod +xr /usr/local/bin/runvm
+chmod +xr /usr/local/bin/sysinfo
