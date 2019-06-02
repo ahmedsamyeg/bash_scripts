349797db2056ab6503a02eef7dd7c0aa2ad98b84
Ahmed Elsayed <me@ahmedsamy.net>
2019-06-01 23:02:12 -0700

    Add other virtual machines options.

--- src/runvm.sh
+++ src/runvm.sh
@@ -14,8 +14,12 @@ then
     echo "   -h, --help, /?     Show instructions."
     echo "   -r, --run   Start the virtual machine in the background."
     echo "   -s, --stop  Stop the virtual machine."
-    echo "   --status list the running virtual machines."
-    
+    echo "   -d, --suspend Suspend the virtual machine"
+    echo "   -p, --pause Pause the virtual machine"
+    echo "   -u, --unpause Unpause the virtual machine"
+    echo "   --reset Reset the virtual machine."
+    echo "   --status List the running virtual machines."
+
 elif [ "$1" == "--status" ]
 then
     /Applications/VMware\ Fusion.app/Contents/Library/vmrun list
@@ -25,4 +29,17 @@ then
 elif [ "$1" == "--run" ] || [ "$1" == "-r" ]
 then
     echo "Starting " $2"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion start "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$2".vmwarevm" nogui
+elif [ "$1" == "--suspend" ] || [ "$1" == "-d" ]
+then
+    echo "Suspending " $2"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion suspend "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$2".vmwarevm" nogui
+elif [ "$1" == "--pause" ] || [ "$1" == "-p" ]
+then
+    echo "Pausing " $2"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion pause "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$2".vmwarevm" nogui
+elif [ "$1" == "--unpause" ] || [ "$1" == "-u" ]
+then
+    echo "Unpausing " $2"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion unpause "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$2".vmwarevm" nogui
+elif [ "$1" == "--reset" ]
+then
+    echo "Resetting " $2"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion reset "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$2".vmwarevm" nogui
+then
 fi
