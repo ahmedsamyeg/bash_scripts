a2f4343a0e8fb0c3a4d42a4c0c7eeec7fc928d58
Ahmed Elsayed <me@ahmedsamy.net>
2019-06-01 19:17:05 -0700

    few refinements

--- src/runvm.sh
+++ src/runvm.sh
@@ -7,12 +7,22 @@
 #   A script to run local vmware fusion virtual machines in the backgound without showing the console in Apple MacOS.
 #
 
-if [ "$1" == "--help" ] || [ "$1" == "--h" ] || [ "$1" == "/?" ] || [ "$1" == "" ]
+if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$1" == "/?" ] || [ "$1" == "" ]
 then
-    echo
-    echo --help, -h, /? : show instructions.
-    echo to run any vm \"./runvm.sh {vm_name_without_extension}\" ie: \"./runvm.sh deb_9_mongodb\"
-    echo
-else
+    echo usage: runvm [options] vm_name_without_extension 
+    echo "  options:"
+    echo "   -h, --help, /?     Show instructions."
+    echo "   -r, --run   Start the virtual machine in the background."
+    echo "   -s, --stop  Stop the virtual machine."
+    echo "   --status list the running virtual machines."
+    
+elif [ "$1" == "--status" ]
+then
+    /Applications/VMware\ Fusion.app/Contents/Library/vmrun list
+elif [ "$1" == "--stop" ] || [ "$1" == "-s" ]
+then
+    echo "Stopping " $1"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion stop "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$1".vmwarevm" nogui
+elif [ "$1" == "--run" ] || [ "$1" == "-r" ]
+then
     echo "Starting " $1"....." & /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion start "/Users/ahmedsamy/Documents/Virtual Machines.localized/"$1".vmwarevm" nogui
 fi
